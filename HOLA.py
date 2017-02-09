c476e5f17a49d61b5b4277f017be71a361676796
Araf Rahman <araft50@yahoo.com>
2017-02-08 19:49:07 -0500

    Branch (#2)
    * Hahaha
    
    * Hahaha

---
+++ HOLA.py
@@ -0,0 +1,14 @@
+while True:
+    name = input("What is your name?")
+    print ("Nice to meet ya "+ name  )
+    color = input("What is your favorite color?")
+    print ("That's a nice color")
+    food = input("What is your favorite food?")
+    print ( food + " sounds yummy")
+    a = input("Enter yes/no to continue")
+    if a=="yes":
+        continue
+    elif a=="no":
+        break
+    else:
+        print("Enter either yes/no")
