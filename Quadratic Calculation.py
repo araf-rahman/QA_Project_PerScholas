57846e580c08d9a15799c35be3fdffc0d3364c76
araf91 <you@example.com>
2017-02-08 20:30:50 -0500

    Created Greeting Program

---
+++ Quadratic Calculation.py
@@ -0,0 +1,7 @@
+def  quadratic ():
+    a = float(input("What is your value of A?"))
+    b = float(input("What is your value of B?"))
+    c = float(input("What is your value of C?"))
+    d = float(input("What is your value of X?"))
+    print("AX**2+BX+C = ",((a*(d**2))+(b*d)+c))
+quadratic()
