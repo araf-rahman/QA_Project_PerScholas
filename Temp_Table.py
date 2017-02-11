18bdc7448967530ad672ffc4b702dc8ce172b700
araf91 <you@example.com>
2017-02-11 14:23:18 -0500

    Created Average game

--- Temp_Table.py
+++ Temp_Table.py
@@ -1,6 +1,7 @@
 start = 0
-end = 100
+end = 101
 increment = 5
+
 def main():
     print('The following table shows conversion of Celsius to Farenheit')
     print( 'between 0 and 100 degrees Celsius.')
@@ -9,6 +10,7 @@ def main():
     print('____________________')
     #convert celsius input to farenheit using loop
     for Celsius in range (start, end, increment):
-        Farenheit = (9 / 5) * Celsius + 32
-        print(Celsius, '\t', format(Farenheit, '.1f'))
+        Farenheit = int((9/ 5) * Celsius + 32)
+        print(Celsius, '\t', format(Farenheit))
 main()
+
