7c8799c050c60818a214b5fa3a472baa769624a9
araf91 <you@example.com>
2017-02-14 19:50:31 -0500

    STC Update

--- STC.py
+++ STC.py
@@ -1,16 +1,21 @@
 print("Welcome to Sentest Counter")
-def main():
-    a = input("Enter yes/no to continue")
-    if a=="yes":
+a = input("Enter yes/no to continue")
+if a=="yes":
         import tkinter as tk
         from tkinter import filedialog
         root = tk.Tk()
         root.withdraw()
         file_path = filedialog.askopenfilename()
-    elif a=="no":
+elif a=="no":
         print("Goodbye")
-    import re
-    print()
-    count = len(re.findall(r'\w+', line))
-    print ("The number of words in this paragraph:", count)
-main()
+numLines = 0
+numWords = 0
+numChars = 0
+
+with open(file_path,"r")as file:
+    for line in file:
+        wordsList = line.split()
+        numLines +=1
+        numWords += len(wordsList)
+        numChars += len(line)
+print (numLines, numWords, numChars)
