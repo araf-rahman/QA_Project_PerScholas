f61b96b21fe81bacbbea90b479cfda241e870b71
araf91 <you@example.com>
2017-02-15 19:35:10 -0500

    STC Update

--- Max & Min.py
+++ Max & Min.py
@@ -5,7 +5,7 @@ while num is None:
     try:
         num = int(ret)
     except ValueError:
-        print ("Invalid input, please enter a digit.")
+        print("Invalid input, please enter a digit.")
 
 print("Enter numbers in array:")
 for i in range(num):
@@ -16,8 +16,19 @@ for i in range(num):
             n = int(ret)
             input_set.append(n)
         except ValueError:
-            print ("Invalid input, please enter a digit.")
+            print("Invalid input, please enter a digit.")
 
-print ("ARRAY: ", input_set)
-print ("Largest number is :", max(input_set))
-print ("Smallest number is :", min(input_set))
+print("ARRAY: ", input_set)
+
+largest=-1
+for i in input_set:
+    if i > largest:
+        largest = i
+smallest = input_set[0]
+for i in range(len(input_set)):
+
+    if input_set[i] < largest:
+        smallest = input_set[i]
+
+print("Largest number is :",largest)
+print("Smallest number is", smallest)
--- STC.py
+++ STC.py
@@ -1,21 +1,23 @@
 print("Welcome to Sentest Counter")
-a = input("Enter yes/no to continue")
-if a=="yes":
-        import tkinter as tk
-        from tkinter import filedialog
-        root = tk.Tk()
-        root.withdraw()
-        file_path = filedialog.askopenfilename()
-elif a=="no":
-        print("Goodbye")
-numLines = 0
-numWords = 0
-numChars = 0
+def main():
+    a = input("Enter yes to continue or no to exit")
+    if a=="yes":
+            import tkinter as tk
+            from tkinter import filedialog
+            root = tk.Tk()
+            root.withdraw()
+            file_path = filedialog.askopenfilename()
+    elif a=="no":
+            print("Goodbye")
+    numLines = 0
+    numWords = 0
+    numChars = 0
 
-with open(file_path,"r")as file:
-    for line in file:
-        wordsList = line.split()
-        numLines +=1
-        numWords += len(wordsList)
-        numChars += len(line)
-print (numLines, numWords, numChars)
+    with open(file_path,"r")as file:
+        for line in file:
+            wordsList = line.split()
+            numLines +=1
+            numWords += len(wordsList)
+            numChars += len(line)
+    print (numLines, numWords, numChars)
+main()
