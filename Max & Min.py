3b77f8dbfa27d8099302294d1884a83bb09cfbbd
araf91 <you@example.com>
2017-02-15 19:36:57 -0500

    STC Update

--- Max & Min.py
+++ Max & Min.py
@@ -18,17 +18,15 @@ for i in range(num):
         except ValueError:
             print("Invalid input, please enter a digit.")
 
-print("ARRAY: ", input_set)
-
 largest=-1
 for i in input_set:
     if i > largest:
         largest = i
+
 smallest = input_set[0]
 for i in range(len(input_set)):
-
     if input_set[i] < largest:
         smallest = input_set[i]
-
+print("ARRAY: ", input_set)
 print("Largest number is :",largest)
-print("Smallest number is", smallest)
+print("Smallest number is:", smallest)
